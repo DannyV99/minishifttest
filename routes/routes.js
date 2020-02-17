10f72ad17cb0c95ad30ba42c542f4549e486b942
Daniel VonMoser <danielvonmoser@Daniels-Air.lan1>
2020-02-16 20:00:09 -0500

    Webhook test

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ var appRouter = function (app) {
     
     
     app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+      res.status(200).send("Welcome to my restful API");
     });
   
     app.get("/user", function (req, res) {
